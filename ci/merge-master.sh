635e0fa47f7d0ceeea6b5e7a198dd55ad59e8b37
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-08-30 08:34:36 +0900

    ci: merge workflow

--- ci/merge-master.sh
+++ ci/merge-master.sh
@@ -4,6 +4,10 @@ set -eux
 
 cd "$(dirname "$0")/.."
 
+if [ "$CIRCLE_BRANCH" = master ]; then
+  exit 0
+fi
+
 ghc exec -- run-ci merge master
 
 ghc exec -- git log -n 5 --pretty=oneline
--- ci/update-pr.sh
+++ ci/update-pr.sh
@@ -4,4 +4,8 @@ set -eu
 
 cd "$(dirname "$0")/.."
 
+if [ "$CIRCLE_BRANCH" != master ]; then
+  exit 0
+fi
+
 ghc exec -- run-ci update-pr --base master
