94d95634ded6841b7a88410bff63d5e4a0af47d7
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-08-30 20:09:34 +0900

    ci: fix how to merge

--- ci/merge-master.sh
+++ ci/merge-master.sh
@@ -4,6 +4,11 @@ set -eux
 
 cd "$(dirname "$0")/.."
 
-ghc exec -- run-ci merge master
+if [ "$CIRCLE_BRANCH" = master ]; then
+  exit 0
+fi
+
+ghc exec -- git fetch origin master
+ghc exec -- env GIT_AUTHOR_NAME=ci-helper GIT_AUTHOR_EMAIL=ci-helper@example.com GIT_COMMITTER_NAME=ci-helper GIT_COMMITTER_EMAIL=ci-helper@example.com git merge --no-edit origin/master
 
 ghc exec -- git log -n 5 --pretty=oneline
