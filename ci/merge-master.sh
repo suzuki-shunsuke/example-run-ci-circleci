4e85baae1c9673bae5fceb9d206c8b528a192cdb
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-08-30 20:09:34 +0900

    ci: fix how to merge

--- ci/merge-master.sh
+++ ci/merge-master.sh
@@ -8,6 +8,7 @@ if [ "$CIRCLE_BRANCH" = master ]; then
   exit 0
 fi
 
-ghc exec -- run-ci merge master
+ghc exec -- git fetch origin master
+ghc exec -- env GIT_AUTHOR_NAME=ci-helper GIT_AUTHOR_EMAIL=ci-helper@example.com GIT_COMMITTER_NAME=ci-helper GIT_COMMITTER_EMAIL=ci-helper@example.com git merge --no-edit origin/master
 
 ghc exec -- git log -n 5 --pretty=oneline
