a62b4e9fa3c93a6bbffd1f2ca3298abe05aa9b88
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-09-06 14:24:43 +0900

    ci: use run-ci

--- ci/install-tools.sh
+++ ci/install-tools.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")/.."
 
 CLAP_VERSION=v0.2.2
 GITHUB_COMMENT_VERSION=v1.9.0
-# RUN_CI_VERSION=v0.1.0-0
+RUN_CI_VERSION=v0.1.0-2
 
 curl --fail -L -o /usr/local/bin/clap https://github.com/suzuki-shunsuke/clap/releases/download/${CLAP_VERSION}/clap_${CLAP_VERSION##v}_linux_amd64
 chmod a+x /usr/local/bin/clap
@@ -16,5 +16,5 @@ chmod a+x /usr/local/bin/github-comment
 github-comment exec -- ln -s /usr/local/bin/github-comment /usr/local/bin/ghc
 
 # ghc exec -- clap install "https://github.com/suzuki-shunsuke/example-run-ci-circleci/releases/download/${RUN_CI_VERSION}/run-ci_${RUN_CI_VERSION}_linux_amd64.tar.gz" run-ci:/usr/local/bin/run-ci
-ghc exec -- curl --fail -L -o /usr/local/bin/run-ci https://github.com/suzuki-shunsuke/example-run-ci-circleci/releases/download/local-build/run-ci-linux
+ghc exec -- clap install "https://github.com/suzuki-shunsuke/run-ci/releases/download/${RUN_CI_VERSION}/run-ci_${RUN_CI_VERSION##v}_linux_amd64.tar.gz" run-ci:/usr/local/bin/run-ci 
 ghc exec -- chmod a+x /usr/local/bin/run-ci
